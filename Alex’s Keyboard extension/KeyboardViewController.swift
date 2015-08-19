bba75f993f6e4465a9e97d0e7d379e8ed0ef5196
Alex Bepple <alex@bepple.de>
2015-08-19 21:36:38 +0200

    type any one letter on click

---
+++ Alex’s Keyboard extension/KeyboardViewController.swift
@@ -0,0 +1,109 @@
+//
+//  KeyboardViewController.swift
+//  Alex’s Keyboard extension
+//
+//  Created by Alex Bepple on 18 Aug 2015.
+//  Copyright (c) 2015 Alex Bepple. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+    init() {
+        super.init(frame: CGRectMake(0, 80, 320, 40))
+        backgroundColor = UIColor(red: 0, green: 0.5, blue:0.5, alpha: 0.5)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        NSLog("myview")
+    }
+}
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+    
+        // Add custom view sizing constraints here
+    }
+    
+    func keyPressed(sender: AnyObject?) {
+        NSLog("bar")
+        let button = sender as! UIButton
+        let title = button.titleForState(.Normal)
+        (textDocumentProxy as! UIKeyInput).insertText(title!)
+    }
+    
+    func someAction(sender:UITapGestureRecognizer){
+        NSLog("baz")
+        (textDocumentProxy as! UIKeyInput).insertText("F")
+    }
+
+    func someAction2(sender:UITapGestureRecognizer){
+        NSLog("baz2")
+        (textDocumentProxy as! UIKeyInput).insertText("G")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        var topRow = UIView(frame: CGRectMake(0, 0, 320, 40))
+        topRow.backgroundColor = UIColor(red: 0, green: 0, blue:0.5, alpha: 0.5)
+        
+        self.view.addSubview(topRow)
+        
+        self.view.addSubview(MyView())
+        
+        let tapRecognizer = UITapGestureRecognizer(target:self, action: "someAction:")
+        topRow.addGestureRecognizer(tapRecognizer)
+        
+        let tapRecognizer2 = UITapGestureRecognizer(target:self, action: "someAction2:")
+        self.view.addGestureRecognizer(tapRecognizer2)
+        self.view.backgroundColor = UIColor(red: 0.5, green: 0, blue:0, alpha: 0.1)
+        
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton.buttonWithType(.System) as! UIButton
+    
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+    
+        self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+    
+        var nextKeyboardButtonLeftSideConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
+        var nextKeyboardButtonBottomConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        self.view.addConstraints([nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+
+    override func textWillChange(textInput: UITextInput) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+
+    override func textDidChange(textInput: UITextInput) {
+        // The app has just changed the document's contents, the document context has been updated.
+    
+        var textColor: UIColor
+        var proxy = self.textDocumentProxy as! UITextDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
+            textColor = UIColor.whiteColor()
+        } else {
+            textColor = UIColor.blackColor()
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+    }
+
+}
---
+++ Alex’s Keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alex’s Keyboard
+//
+//  Created by Alex Bepple on 18 Aug 2015.
+//  Copyright (c) 2015 Alex Bepple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
