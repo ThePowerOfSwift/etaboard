e42b92da59b57946483b376fdf90ecd73b7d288f
Alex Bepple <alex@bepple.de>
2015-09-09 15:43:04 +0200

    o limit text view of container app to the screen

--- Alex’s Keyboard/ViewController.swift
+++ Alex’s Keyboard/ViewController.swift
@@ -4,7 +4,13 @@ class ViewController: UIViewController {
     @IBOutlet var textView: UITextView!
     
     override func viewDidLoad() {
+        setBorder(on: textView)
         textView.becomeFirstResponder()
     }
-}
 
+    func setBorder(on view: UITextView) {
+        view.layer.borderColor = UIColor.darkGrayColor().CGColor
+        view.layer.borderWidth = 0.5
+        view.layer.cornerRadius = 5.0
+    }
+}
