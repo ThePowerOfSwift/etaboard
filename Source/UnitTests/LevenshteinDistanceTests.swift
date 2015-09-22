18019203b60ab00ff552efb554c7a2f637791839
Alex Bepple <alex@bepple.de>
2015-09-22 19:40:28 +0200

    ~ extend Levenshtein tests to cover non-empty strings

--- Source/UnitTests/LevenshteinDistanceTests.swift
+++ Source/UnitTests/LevenshteinDistanceTests.swift
@@ -3,9 +3,15 @@ import Nimble
 
 class LevenshteinDistanceTests: XCTestCase {
     
-    func test() {
+    func testNonEmptyStrings() {
         expect(levenshteinDistance("a", s2: "a")) == 0
         expect(levenshteinDistance("a", s2: "b")) == 1
+        expect(levenshteinDistance("a",  s2: "ab")) == 1
+        expect(levenshteinDistance("ab", s2: "a")) == 1
+    }
+
+    func testEmptyStrings() {
+        expect(levenshteinDistance("", s2: "")) == 0
         expect(levenshteinDistance("",  s2: "a")) == 1
         expect(levenshteinDistance("a", s2: "")) == 1
     }
