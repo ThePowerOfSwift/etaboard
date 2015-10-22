c2a87e104d1e52d714d5d45e8677395c43a00fa3
Alex Bepple <alex@bepple.de>
2015-10-22 20:45:55 +0200

    ~ move the last dictionary to the bundle

--- Source/KeyboardExtension/SuggesterWithDictionaries.swift
+++ Source/KeyboardExtension/SuggesterWithDictionaries.swift
@@ -6,37 +6,16 @@ class SuggesterWithDictionaries {
     class func createSuggester() -> Suggester {
         let suggester = Suggester()
         
-        ["misc", "de"].forEach { dictionary in
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-                loadSuggestionsFromDictionary(dictionary)
-            })
-        }
-        
-        let paths = NSBundle.mainBundle().pathsForResourcesOfType("txt", inDirectory: "dictionaries.bundle")
+        let paths = NSBundle.mainBundle().pathsForResourcesOfType("txt", inDirectory: "Dictionaries.bundle")
         paths.forEach { path in
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
                 loadSuggestionsFromDictionaryAt(path)
             })
         }
-
         
         return suggester
     }
     
-    private static func loadSuggestionsFromDictionary(basename: String) {
-        guard let path = NSBundle.mainBundle().pathForResource(basename, ofType: "txt") else {
-            NSLog("could not find dictionary '\(basename)'")
-            return
-        }
-        do {
-            let dictionaryAsString = try String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
-            let words = dictionaryAsString.split("\n")
-            addSuggestions(words, from: basename)
-        } catch _ as NSError {
-            NSLog("could not load dictionary from path \(path)")
-        }
-    }
-
     private static func loadSuggestionsFromDictionaryAt(path: String) {
         let basename = path.split("/").last
         do {
