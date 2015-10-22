063766ea20909189edda355f3881877095926968
Alex Bepple <alex@bepple.de>
2015-10-22 20:08:49 +0200

    ~ move en dictionary to Dictionaries.bundle

--- Source/KeyboardExtension/SuggesterWithDictionaries.swift
+++ Source/KeyboardExtension/SuggesterWithDictionaries.swift
@@ -6,11 +6,19 @@ class SuggesterWithDictionaries {
     class func createSuggester() -> Suggester {
         let suggester = Suggester()
         
-        ["misc", "de", "top10000en"].forEach { dictionary in
+        ["misc", "de"].forEach { dictionary in
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
                 loadSuggestionsFromDictionary(dictionary)
             })
         }
+        
+        let paths = NSBundle.mainBundle().pathsForResourcesOfType("txt", inDirectory: "dictionaries.bundle")
+        paths.forEach { path in
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                loadSuggestionsFromDictionaryAt(path)
+            })
+        }
+
         
         return suggester
     }
@@ -28,9 +36,20 @@ class SuggesterWithDictionaries {
             NSLog("could not load dictionary from path \(path)")
         }
     }
-    
-    static func addSuggestions(words: [String], from dictionary: String) {
-        NSLog("entries in dictionary '\(dictionary)': \(words.count)")
+
+    private static func loadSuggestionsFromDictionaryAt(path: String) {
+        let basename = path.split("/").last
+        do {
+            let dictionaryAsString = try String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+            let words = dictionaryAsString.split("\n")
+            addSuggestions(words, from: basename)
+        } catch _ as NSError {
+            NSLog("could not load dictionary from path \(path)")
+        }
+    }
+
+    static func addSuggestions(words: [String], from dictionary: String? = "<unknown>") {
+        NSLog("entries in dictionary '\(dictionary!)': \(words.count)")
         instance.add(words)
         NSLog("suggester size: \(instance.size)")
     }
