2f0c478adb881c3f374ed9206ca464d2f90f03a9
Alex Bepple <alex@bepple.de>
2015-10-29 20:24:23 +0100

    ~ dry

--- Source/KeyboardExtension/SuggesterWithDictionaries.swift
+++ Source/KeyboardExtension/SuggesterWithDictionaries.swift
@@ -6,23 +6,25 @@ class SuggesterWithDictionaries {
     class func createSuggester() -> Suggester {
         let suggester = Suggester()
         
-        let paths = NSBundle.mainBundle().pathsForResourcesOfType("txt", inDirectory: "Dictionaries.bundle")
+        loadDictionaries(from: ".", with: suggester.addUnknownLengths)
+        loadDictionaries(from: "de", with: suggester.addSameLength)
+        
+        return suggester
+    }
+    
+    private static func loadDictionaries(from pathInBundle: String,
+        with functor: [String] -> ()) {
+            
+        let bundlePath = "Dictionaries.bundle"
+        let directory = NSURL.fileURLWithPathComponents([bundlePath, pathInBundle])?.path
+        let paths = NSBundle.mainBundle().pathsForResourcesOfType("txt",
+            inDirectory: directory)
         NSLog("paths: \(paths)")
         paths.forEach { path in
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-                loadSuggestionsFromDictionaryAt(path) |> instance.addUnknownLengths
+                loadSuggestionsFromDictionaryAt(path) |> functor
             })
         }
-
-        let de_paths = NSBundle.mainBundle().pathsForResourcesOfType("txt", inDirectory: "Dictionaries.bundle/de")
-        NSLog("de paths: \(de_paths)")
-        de_paths.forEach { path in
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-                loadSuggestionsFromDictionaryAt(path) |> instance.addSameLength
-            })
-        }
-
-        return suggester
     }
     
     private static func loadSuggestionsFromDictionaryAt(path: String) -> [String] {
