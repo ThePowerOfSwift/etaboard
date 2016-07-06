9857301c3410e822b4a0e8dc08107d18ede54509
Alex Bepple <alex@bepple.de>
2016-07-06 17:37:54 +0200

    ~ move class to location of use

--- Source/UnitTests/SuggesterTests.swift
+++ Source/UnitTests/SuggesterTests.swift
@@ -49,3 +49,26 @@ class SuggesterTests: XCTestCase {
         return collector.getBestSuggestion()
     }
 }
+
+
+private class OneSuggestionCollector: SuggestionsCollector {
+    private var bestDistance = Distance.max
+    private var bestSuggestion = ""
+    
+    var boundary: Distance { return bestDistance }
+    
+    func addSuggestion(suggestion: String, distance: Distance) {
+        if distance < bestDistance {
+            bestDistance = distance
+            bestSuggestion = suggestion
+        }
+    }
+    
+    func mapSuggestions(functor: String -> String) {
+        bestSuggestion = functor(bestSuggestion)
+    }
+    
+    func getBestSuggestion() -> String {
+        return bestSuggestion
+    }
+}
